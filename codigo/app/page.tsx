1d94bb7b9bcde64e46ab3fb2201952481621d24a
Fiosorio <fi.osorio@duocuc.cl>
2026-09-13 21:22:38 -0300

    primera actualizacion front-end

---
+++ codigo/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+import Navbar1 from "../components/Navbar1";
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar1></Navbar1>
+    </div>
+  );
+}
